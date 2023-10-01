a6aa8c6f0db63069ee02b5e4b5cbbeae42c016d7
ben <greatmaharg@gmail.com>
2023-09-30 21:19:56 -0400

    Updated library; basics of accel data input

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,14 @@
 #include "pindefs.h"
 
 #define PACKET_SIZE 64
-#define SAMPLE_RATE 500
+#define SAMPLE_RATE 20
 // TODO: variable sample rate?
 // If we record at maximum rate all the time we run out of flash very quickly
 
 TwoWire i2c(P_SDA, P_SCL);
 SPIClass spi(P_MISO, P_MOSI, P_SCK);
+
+LSM6DSO32 imu;
 
 enum class FlightState : uint8_t {
     ERROR,
@@ -49,13 +51,52 @@ static_assert(1e6/SAMPLE_RATE <= std::numeric_limits<uint16_t>::max(),
 // Flight state variables
 uint32_t lastPacketTime; // us
 float tempReading; // deg C
+
+uint32_t currentIMUTime;
 VectorF accelReading; // m/s^2
-float altReading; // m
 VectorF gyroReading; // rad/s
+
+float altReading; // m
 FlightState state = FlightState::ERROR;
 
-VectorF vel;
-QuatF orientation;
+VectorF vel(0, 0, 0);
+VectorF grav(0, 0, 0);
+QuatF orientation(1, 0, 0, 0);
+
+volatile bool imuWaiting = false;
+volatile bool higWaiting = false;
+volatile bool baroWaiting = false;
+
+void isr_imu() { imuWaiting = true; currentIMUTime = micros(); }
+void isr_hig() { higWaiting = true; }
+
+void readIMU() {
+    static uint32_t lastIMUTime = micros();
+
+    accelReading = imu.getAccel();
+    gyroReading = imu.getGyro();
+
+    // TODO: sensor fusion/Kalman filter?
+    float timestep = (currentIMUTime-lastIMUTime)/1e6f;
+    VectorF rot_step = gyroReading * timestep;
+    orientation = orientation * QuatF::fromAngleAxis(rot_step);
+    
+    // TODO: make derived velocity suck less
+    vel = vel + (accelReading.rotate(~orientation) - grav)*timestep;
+    lastIMUTime = currentIMUTime;
+}
+
+void readHiG() {
+    // TODO
+}
+
+void readBaro() {
+    // TODO
+}
+
+void advanceState() {
+    // TODO
+}
 
 DataPacket recordState() {
     uint32_t time = micros();
@@ -73,12 +114,16 @@ DataPacket recordState() {
     return d;
 }
 
-bool imuWaiting = false;
-bool higWaiting = false;
-bool baroWaiting = false;
+void logToFlash() {
+    DataPacket dp = recordState();
 
-void isr_imu() { imuWaiting = true; }
-void isr_hig() { higWaiting = true; }
+    VectorF vec = dp.accel.rotate(~dp.orientation);
+    Serial.printf("<%f, %f, %f, %f> <%f, %f, %f>\n",
+            dp.orientation.w, dp.orientation.x,
+            dp.orientation.y, dp.orientation.z,
+            dp.vel.x, dp.vel.y, dp.vel.z);
+    // TODO: actual logging
+}
 
 // MAIN PROGRAM
 
@@ -90,6 +135,22 @@ void setup() {
     attachInterrupt(INT_HIG, isr_hig, RISING);
     // TODO: interrupt for baro (the BMP280 doesn't have one so waiting for
     //       BMP390 or using MPL3115)
+
+    Serial.begin(4000000);
+    while(!Serial);
+
+    while(!imu.begin(&i2c, LSM_I2C_LO)) {
+        Serial.println("IMU init failed!\n");
+        delay(1000);
+    }
+    imu.setInt1(LSM_INT1_DRDY_G);
+    imu.startGyro(LSM_RANGE_2000DS, LSM_SRATE_1666);
+    imu.startAccel(LSM_RANGE_32G, LSM_SRATE_1666, 0);
+    for(int i = 0; i < 100; i++) {
+        delay(10);
+        grav = grav + imu.getAccel()*0.01;
+    }
+    grav = grav * (LSM_GRAV / grav.mag());
 }
 
 void loop() {
@@ -97,12 +158,16 @@ void loop() {
     // Right now a fast enough IMU with a slow enough CPU can in theory stall
     // the loop forever and never allow it to record a packet
     if(imuWaiting) {
-        // TODO: Read IMU data
+        imuWaiting = false;
+        readIMU();
     } else if(higWaiting) {
-        // TODO: Read high-G data
+        higWaiting = false;
+        readHiG();
     } else if(baroWaiting) {
-        // TODO: Read baro data
+        baroWaiting = false;
+        readBaro();
     } else if(micros()-lastPacketTime > uint32_t(1e6 / SAMPLE_RATE)) {
-        // TODO: Log data to flash
+        advanceState();
+        logToFlash();
     }
 }
